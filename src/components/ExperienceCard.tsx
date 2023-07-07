0dabd7e0fce937ff4b64515f9441a2aec2814aa4
Brian Steele <brian.steele@hey.com>
2023-07-06 18:03:39 -0600

    adding experience and tags

--- src/components/ExperienceCard.tsx
+++ src/components/ExperienceCard.tsx
@@ -4,6 +4,7 @@ type Props = {
   company: string
   role: string
   tags: readonly string[]
+  description?: string
 }
 
 export function ExperienceCard({
@@ -12,21 +13,31 @@ export function ExperienceCard({
   company,
   role,
   tags,
+  description,
 }: Props) {
   return (
-    <div className="flex gap-6 max-w-lg h-80  hover:bg-santa-gray/10 duration-500 transition-colors p-4 rounded-md cursor-pointer">
+    <div className="flex gap-6 max-w-xl  hover:bg-santa-gray/10 duration-500 transition-colors p-4 rounded-md cursor-pointer">
       <div className="w-1/4">
         <p className="uppercase text-sm font-semibold text-end">{`${startDate}-${endDate}`}</p>
       </div>
-      <div className="w-3/4">
-        <h3 className="text-lg font-semibold">
+      <div className="w-3/4 flex flex-col gap-4">
+        <h3 className="text-lg font-semibold leading-5">
           <span className="text-santa-red">{role}</span>
           {` - ${company}`}
         </h3>
-        <p className="font-small">
-          A whole bunch of stuff about what I do in this role. Focus on outcomes
-          and all the ways I make the company better
-        </p>
+        <p className="font-small">{description}</p>
+        <div className="flex gap-4 flex-wrap mt-3">
+          {tags.map((t, idx) => {
+            return (
+              <span
+                className="px-2 py-1 bg-santa-juniper rounded-xl text-santa-plum"
+                key={`${t}-${idx}`}
+              >
+                {t}
+              </span>
+            )
+          })}
+        </div>
       </div>
     </div>
   )
--- src/data/experience.ts
+++ src/data/experience.ts
@@ -5,6 +5,7 @@ export const experience = [
     company: 'Honor Education',
     role: 'Lead Web Engineer',
     tags: ['TypeScript', 'Next', 'Fullstack', 'Postgres', 'AWS'],
+    description: `Working as a founding web-engineer responsible for building the apps that instituions use to craft their course content as well as building the apps students use to consume. Spent most of my time building in Next with TypeScript and crafting APIs using Node and Java hitting a Postgres DB all deployed on AWS infra.`,
   } as const,
   {
     startDate: '2020',
@@ -12,6 +13,7 @@ export const experience = [
     company: 'DocuSign',
     role: 'Lead Web Engineer',
     tags: ['TypeScript', 'React', 'Component Library'],
+    description: `Leading a team of engineers who built a React component library used to power apps across the enterprise. Led the transition to TypeScript which led to easier refactors with less regressions. Also, implemented a robust CI/CD pipeline that reduced developer effort required for releasing as well as making it easier for the flaghsip apps to upgrade. Build out a hiring pipeline and helped build the team culture.`,
   } as const,
   {
     startDate: '2019',
@@ -19,6 +21,7 @@ export const experience = [
     company: 'VSCO',
     role: 'Senior Engineering Manager',
     tags: ['TypeScript', 'React', 'Leadership'],
+    description: `Led migration from Flow to TypeScript. Led integration of new HR software into website to automate job postings. Worked with product/creative to implement custom CMS for company blog.`,
   } as const,
   {
     startDate: '2013',
@@ -26,5 +29,6 @@ export const experience = [
     company: 'Ameriprise Financial',
     role: 'Director - Front End Engineering',
     tags: ['TypeScript', 'JavaScript', 'Backbone', 'React', 'Leadership'],
+    description: `Started as only front-end engineer in organization and grew team to 25 engineers. Team was responsible for building out all advisor facing applications used to create and maintain business. Led migration to SPA architecture. Implemented hiring best practices. Worked with staff engineers to utilize TypeScript in the codebase. Led enterprise in choosing React as the standard framework`,
   } as const,
 ]
