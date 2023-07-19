f81dda2d3d99512538d4a2751ed5bab02a9bf369
Brian Steele <brian.steele@hey.com>
2023-07-19 10:47:08 -0600

    adding a nice glow

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-santa-plum leading-relaxed`}>
+      <body className={`${inter.className} bg-slate-900 leading-relaxed`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,41 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import { Header } from '@/components'
 import { ContentContainer } from '@/components/ContentContainer'
 
 export default function Home() {
+  const [mousePosition, setMousePosition] = useState<[number, number] | null>(
+    null
+  )
+
+  useEffect(() => {
+    const handler = (e: MouseEvent) => {
+      const pos = [e.clientX, e.clientY] as [number, number]
+      setMousePosition(pos)
+    }
+
+    window.addEventListener('mousemove', handler)
+
+    return () => {
+      window.removeEventListener('mousemove', handler)
+    }
+  }, [])
+
+  const styleObj = {
+    background: `radial-gradient(600px at ${mousePosition?.[0]}px ${mousePosition?.[1]}px, rgba(216, 211, 219, 0.15), transparent 80%)`,
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-start  px-8 py-8 md:p-24 gap-12 md:flex-row">
-      <Header />
-      <ContentContainer />
-    </main>
+    <div className="relative">
+      <div
+        style={styleObj}
+        className="pointer-events-none  inset-0 z-30 transition duration-300 lg:absolute"
+      />
+      <main className="flex min-h-screen flex-col items-start  px-8 py-8 md:p-24 gap-12 md:flex-row">
+        <Header />
+        <ContentContainer />
+      </main>
+    </div>
   )
 }
