7f3974dc1ea495dcc9c424d4eddb5917372f8854
Brian Steele <gobriansteele@gmail.com>
2024-06-23 07:06:24 -0400

    Fix prettier settings

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,30 @@
 'use client'
 
-import { useAnimate, useInView, stagger, AnimationSequence } from 'framer-motion'
+import { useAnimate, AnimationSequence } from 'framer-motion'
 import { Avatar } from './Avatar'
 import { SocialContacts } from './SocialContacts'
 import { useEffect } from 'react'
 
 const sequence = [
-  ["h1", { opacity: [0, 1] }, { duration: .5, at: .5 }],
-  ["h2", { opacity: [0, 1] }, { duration: .5, at: 1}],
-  ["p", { opacity: [0, 1] }, { duration: .5, at: 1}],
-  ["#social", { opacity: [0, 1] }, {duration:.5, at: 1}],
-  ["img", { opacity: [0, 1] }, {duration:.5, at: 1.5}],
+  ['h1', { opacity: [0, 1] }, { duration: 0.5, at: 0.5 }],
+  ['h2', { opacity: [0, 1] }, { duration: 0.5, at: 1 }],
+  ['p', { opacity: [0, 1] }, { duration: 0.5, at: 1 }],
+  ['#social', { opacity: [0, 1] }, { duration: 0.5, at: 1 }],
+  ['img', { opacity: [0, 1] }, { duration: 0.5, at: 1.5 }],
 ] satisfies AnimationSequence
 
 export function Header() {
   const [scope, animate] = useAnimate()
 
-  const isInView = useInView(scope, {
-    once: true
-  })
-
-  useEffect(()=>{
+  useEffect(() => {
     animate(sequence)
   }, [animate])
 
   return (
-    <header ref={scope} className="text-santa-gray flex flex-col justify-between w-full md:flex-col-reverse gap-12 md:w-1/3 md:sticky md: top-0 py-8">
+    <header
+      ref={scope}
+      className="text-santa-gray flex flex-col justify-between w-full md:flex-col-reverse gap-12 md:w-1/3 md:sticky md: top-0 py-8"
+    >
       <div className="flex justify-center ">
         <Avatar />
       </div>
